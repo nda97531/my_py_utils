6505c7a4af4e2bf550d9542fca6f37ac1ea43632
Nguyễn Đức Anh <42064053+nda97531@users.noreply.github.com>
2024-09-27 01:14:30 +0100

    Update string_utils.py

--- my_py_utils/string_utils.py
+++ my_py_utils/string_utils.py
@@ -13,6 +13,16 @@ def rreplace(s: str, old: str, new: str, occurrence: int = 1):
     li = s.rsplit(old, occurrence)
     return new.join(li)
 
+def print_dict_with_precision(data_dict: dict, decimal_points: int):
+    """
+    Prints the dictionary in one row with specified decimal points for float values.
+    
+    Args:
+        data_dict (dict): Dictionary with string keys and float values.
+        decimal_points (int): Number of decimal points to display for float values.
+    """
+    formatted_items = [f'{key}: {value:.{decimal_points}f}' for key, value in data_dict.items()]
+    print(", ".join(formatted_items))
 
 if __name__ == '__main__':
     print(rreplace(
